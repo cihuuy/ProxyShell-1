3b65cd1d88863f199d6ce066b85c10c70fe351b1
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-06-24 13:52:18 +0700

    Update shodan-query.py

--- shodan-query.py
+++ shodan-query.py
@@ -3,7 +3,7 @@ import shodan
 requests.packages.urllib3.disable_warnings()
 
 
-API_KEY=""
+API_KEY="g6AfgRJTNbIAQ8HlpP9ygqoifYAj3zXT"
 SEARCH_FOR="http.favicon.hash:3A-855213521"
 
 f=open("url.txt","a")
@@ -16,7 +16,7 @@ result = api.search(SEARCH_FOR,limit=1000)
 
 for service in result['matches']:
     IP = service['ip_str']
-    url="https://{}".format(IP)
+    url="https://www.shodan.io/search?query=http.favicon.hash%3A-855213521".format(IP)
     f.writelines(url+"\n")
 
 
